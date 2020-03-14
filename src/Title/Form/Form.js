d56bc20445076281117a1e3446ca491df4bde998
Baktygul <baktygul160799@gmail.com>
2020-03-14 20:55:49 +0600

    aa

--- src/Title/Form/Form.js
+++ src/Title/Form/Form.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './Form.css';
 class Form extends Component {
-
+  
   render() {
     const { title, desc, myChangeHandler, handleToggleClickClean, handleSubmit } = this.props;
     return (
